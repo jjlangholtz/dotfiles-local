34eda05364e79acf42812317763ed761ab74a7a8
Josh Langholtz <jjlangholtz@gmail.com>
2016-05-03 15:06:45 -0400

    Show hidden files in Finder by default

--- scripts/osx.sh
+++ scripts/osx.sh
@@ -2,3 +2,6 @@
 
 # Use all F1, F2, etc. keys as standard function keys
 defaults write -g com.apple.keyboard.fnState -bool true
+
+# Show hidden files in Finder by default
+defaults write -g com.apple.finder AppleShowAllFiles YES
